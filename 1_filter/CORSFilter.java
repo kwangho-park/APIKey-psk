a6703cc616cf609f58fb2d578f6ca27ba0369481
박광호 <44250982+kwangho-park@users.noreply.github.com>
2023-08-26 23:26:00 +0900

    CORS 소스 추가

---
+++ 1_filter/CORSFilter.java
@@ -0,0 +1,20 @@
+package kr.com.dreamsecurity.hotp.filter;
+
+// 크로스사이트 스크립트 필터
+//public class CORSFilter implements Filter {
+//	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+//			throws IOException, ServletException {
+//		HttpServletResponse response = (HttpServletResponse) res;
+//		response.setHeader("Access-Control-Allow-Origin", "*");
+//		response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
+//		response.setHeader("Access-Control-Max-Age", "3600");
+//		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+//		chain.doFilter(req, res);
+//	}
+//
+//	public void init(FilterConfig filterConfig) {
+//	}
+//
+//	public void destroy() {
+//	}
+//}
